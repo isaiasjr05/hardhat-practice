8643f2389940c6702d8033c93887f8e4aed088f9
Isaias Glemao Jr <151098248+isaiasjr05@users.noreply.github.com>
2024-09-14 00:13:55 +0800

    sample token contract

--- test/Token.js
+++ test/Token.js
@@ -1,5 +1,6 @@
 // This is an example test file. Hardhat will run every *.js file in `test/`,
 // so feel free to add new ones.
+
 const { ethers } = require('hardhat');
 // Hardhat tests are normally written with Mocha and Chai.
 
